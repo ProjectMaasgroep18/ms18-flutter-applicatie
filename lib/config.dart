1ddd9b5fb3db2a161fe81894583ad6005d311a29
JBH-JORRIT <40595903+Joossensei@users.noreply.github.com>
2023-10-09 20:04:02 +0200

    Starting with a default menu
    This should contain the needed menu items. Also edited the default config

--- lib/config.dart
+++ lib/config.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
 //Colors
-const Color mainColor = Color(0xFFe02b20);
-const Color secondColor = Colors.black;
+const Color mainColor = Color(0xFFf24300);
+const Color secondColor = Color(0xFFffc4ae);
 const Color dangerColor = Color(0xffEA5A5A);
 const Color successColor = Color(0xff6cc070);
 const Color backgroundColor = Color(0xfffafafa);
 const Color textColor = Colors.black;
-const Color textColorOnSecondColor = Color(0xFFe02b20);
+const Color textColorOnSecondColor = Color(0xFFf24300);
 const Color textColorOnMainColor = Colors.white;
 const Color mainButtonColor = mainColor;
 
-//Logo's
+/*Logo's  -  Hebben we (nog) niet
 const String fullLogo = 'images/Obladi logo.png';
 const String smallLogo = 'images/Obladi favicon.png';
+*/
 
 //Input styles
 const TextStyle labelTextStyle =
-    TextStyle(fontWeight: FontWeight.w600, fontSize: 15, color: textColor);
+TextStyle(fontWeight: FontWeight.w600, fontSize: 15, color: textColor);
 const Icon dropdownIcon = Icon(
   Icons.arrow_downward,
   size: 20,
@@ -32,14 +33,14 @@ const InputDecoration inputFieldStyle = InputDecoration(
     fillColor: backgroundColor,
     isDense: true,
     contentPadding:
-        EdgeInsets.only(top: inputHeight, bottom: inputHeight, right: 10),
+    EdgeInsets.only(top: inputHeight, bottom: inputHeight, right: 10),
     border: OutlineInputBorder(
       borderSide: BorderSide(width: 2, color: Colors.red),
     ),
     errorStyle: TextStyle(color: dangerColor),
     prefixText: '   ');
 final InputDecoration inputFieldStyleDropdown = inputFieldStyle.copyWith(
-  contentPadding: EdgeInsets.only(
+  contentPadding: const EdgeInsets.only(
       top: inputHeight - 2.75, bottom: inputHeight - 2.75, right: 10),
 );
 
@@ -65,22 +66,20 @@ const double buttonHeight = 35;
 const double itemHeight = 11;
 
 //Navigator key
-final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-//From here down you got stuff for roles.dart
+//From here down you got stuff for roles
 
-// enum of the roles.dart so you can make a spelling error
+// enum of the roles so you can make a spelling error
 enum RolesNames {
-  Nothing,
-  Read,
-  Write,
-  Delete,
+  Customer,
+  StockUser,
+  Admin,
 }
 
 //user role values to not type them wrong but to use the variable
 const Map<RolesNames, int> roles = {
-  RolesNames.Nothing: 0,
-  RolesNames.Read: 1,
-  RolesNames.Write: 2,
-  RolesNames.Delete: 3,
+  RolesNames.Customer: 0,
+  RolesNames.StockUser: 1,
+  RolesNames.Admin: 2,
 };
--- lib/menu.dart
+++ lib/menu.dart
@@ -18,85 +18,29 @@ class Menu extends StatelessWidget {
       page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
     ),
     menuItem.MenuItem(
-        text: 'Activiteiten',
-        icon: Icons.event,
+        text: 'Voorraad',
+        icon: Icons.add_chart,
         page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
         roles: [Roles.Admin, Roles.Subadmin]),
     menuItem.MenuItem(
-        text: 'Gebruikers',
-        icon: Icons.people,
-        page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-        roles: [Roles.Admin, Roles.Subadmin]),
+        text: 'Foto\'s',
+        icon: Icons.photo,
+        page: MaterialPageRoute(builder: (context) => Menu(child: Container()))),
     menuItem.MenuItem(
-        text: 'Berichten',
+        text: 'Declaraties',
         icon: Icons.message,
         page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
         roles: [Roles.Admin, Roles.Subadmin]),
     menuItem.MenuItem(
-      text: 'KlantenService',
-      icon: Icons.contact_page,
+      text: 'Agenda',
+      icon: Icons.calendar_month,
       page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
     ),
     menuItem.MenuItem(
-      text: 'Locaties',
-      icon: Icons.location_on,
+      text: 'Google Maps',
+      icon: Icons.map_outlined,
       page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
     ),
-    menuItem.MenuItem(
-      text: 'Organisatoren',
-      icon: Icons.person,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Gastvrouw/heer',
-      icon: Icons.person_add,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Categorieën',
-      icon: Icons.category,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Abonnementen',
-      icon: Icons.military_tech,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Betalingen',
-      icon: Icons.payment,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-        text: 'Email Dashboard',
-        icon: Icons.mail,
-        page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-        roles: [Roles.Admin, Roles.Subadmin]),
-    menuItem.MenuItem(
-      text: 'Kortingscodes',
-      icon: Icons.redeem,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Gebruikte Kortingscodes',
-      icon: Icons.credit_score,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-      text: 'Gebeurtenissen',
-      icon: Icons.history,
-      page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-    ),
-    menuItem.MenuItem(
-        text: 'Profiel',
-        icon: Icons.settings,
-        page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-        roles: [Roles.Admin, Roles.Subadmin]),
-    menuItem.MenuItem(
-        text: 'Uitloggen',
-        icon: Icons.logout,
-        page: MaterialPageRoute(builder: (context) => Menu(child: Container())),
-        roles: [Roles.Admin, Roles.Subadmin]),
   ];
 
   Menu({required this.child, this.title});
@@ -112,7 +56,7 @@ class Menu extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(vertical: 20),
           child: Image.asset(
-            fullLogo,
+            "",
             height: 60,
             width: 200,
           ),
@@ -139,7 +83,7 @@ class Menu extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(vertical: 20),
           child: Image.asset(
-            smallLogo,
+            "",
             height: 30,
             width: 30,
           ),
