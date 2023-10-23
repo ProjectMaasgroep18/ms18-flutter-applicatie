68535fadb824ffd10dcb2e30867f359e128676ed
Tim Grootscholten <timgrootscholtentim@gmail.com>
2023-10-23 17:45:19 +0200

    add api wip

--- lib/Calendar/calendar.dart
+++ lib/Calendar/calendar.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import '../menu.dart';
@@ -8,13 +11,73 @@ class Calendar extends StatefulWidget {
   @override
   State<Calendar> createState() => CalendarState();
 }
+
 const scheduleViewSettings = ScheduleViewSettings(
-  monthHeaderSettings: MonthHeaderSettings(
-    backgroundColor: Color.fromARGB(255, 227, 233, 255)
-  ),
+  monthHeaderSettings:
+      MonthHeaderSettings(backgroundColor: Color.fromARGB(255, 227, 233, 255)),
 );
 
 class CalendarState extends State<Calendar> {
+  List<Event> events = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataAsync();
+  }
+
+  Future<void> _fetchDataAsync() async {
+    final data = await _getDataSourceAsync();
+    setState(() {
+      events = data;
+    });
+  }
+
+  Future<List<Event>> _getDataSourceAsync() async {
+    final client = HttpClient();
+    final List<Event> events = [];
+    print("helllloooo");
+    try {
+      final request = await client
+          .getUrl(Uri.parse('https://localhost:7059/Calendar/stam'));
+      final response = await request.close();
+      print("kippp");
+
+      if (response.statusCode == HttpStatus.ok) {
+        final responseBody = await response.transform(utf8.decoder).join();
+        final data = json.decode(responseBody);
+
+        if (data is List) {
+          for (final eventData in data) {
+            print(eventData['title']);
+            final eventName = eventData['title'];
+            final startDateTime = DateTime.parse(eventData['starDateTime']);
+            final endDateTime = DateTime.parse(eventData['endDateTime']);
+            final isAllDay = endDateTime.isBefore(startDateTime);
+            final event = Event(
+              eventName,
+              startDateTime,
+              endDateTime,
+              Colors.blue, // Vervang dit door de juiste achtergrondkleur
+              isAllDay,
+            );
+            events.add(event);
+          }
+        } else {
+          print('Ongeldige JSON-response.');
+        }
+      } else {
+        print('Fout bij het ophalen van gegevens: ${response.statusCode}');
+      }
+    } catch (exception) {
+      print('Er is een fout opgetreden: $exception');
+    } finally {
+      client.close();
+    }
+
+    return events;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Menu(
@@ -23,29 +86,15 @@ class CalendarState extends State<Calendar> {
         timeSlotViewSettings: const TimeSlotViewSettings(
           timeFormat: 'HH:mm',
         ),
-        dataSource: MeetingDataSource(_getDataSource()),
+        dataSource: EventDataSource(events),
         scheduleViewSettings: scheduleViewSettings,
-
       ),
     );
   }
-
-  List<Meeting> _getDataSource() {
-    // sample data
-    final List<Meeting> meetings = <Meeting>[];
-    final DateTime today = DateTime.now();
-    final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0);
-    final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(Meeting('Voorbeeld 1', startTime, endTime, const Color.fromARGB(255, 102, 140, 217), false));
-    meetings.add(Meeting('Voorbeeld 2', startTime.add(const Duration(hours: 2)), endTime.add(const Duration(hours: 2)), const Color.fromARGB(255, 102, 140, 217), false));
-    meetings.add(Meeting('Voorbeeld 3', startTime.add(const Duration(hours: 24)), endTime.add(const Duration(hours: 24)), const Color.fromARGB(255, 250, 209, 99), false));
-
-    return meetings;
-  }
 }
 
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Meeting> source) {
+class EventDataSource extends CalendarDataSource {
+  EventDataSource(List<Event> source) {
     appointments = source;
   }
 
@@ -75,8 +124,8 @@ class MeetingDataSource extends CalendarDataSource {
   }
 }
 
-class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+class Event {
+  Event(this.eventName, this.from, this.to, this.background, this.isAllDay);
 
   String eventName;
   DateTime from;
