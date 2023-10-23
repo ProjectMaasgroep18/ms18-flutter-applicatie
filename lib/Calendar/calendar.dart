014b88d4daad621ee628f3f527532a15db02513d
Vince <50339448+Vinciepincie@users.noreply.github.com>
2023-10-23 14:43:03 +0200

    initialize syncfusion and start layout

--- lib/Calendar/calendar.dart
+++ lib/Calendar/calendar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import '../menu.dart';
 
 class Calendar extends StatefulWidget {
@@ -12,6 +12,68 @@ class Calendar extends StatefulWidget {
 class CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
-    return Menu(child: Text("Ga vanaf hier verder"));
+    return Menu(
+      child: SfCalendar(
+        view: CalendarView.schedule,
+        dataSource: MeetingDataSource(_getDataSource()),
+        monthViewSettings: MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+          showAgenda: true,
+        ),
+      ),
+    );
   }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(Meeting('Voorbeeld 1', startTime, endTime, const Color(0xFF0F8644), false));
+    meetings.add(Meeting('Voorbeeld 2', startTime.add(const Duration(hours: 2)), endTime.add(const Duration(hours: 2)) , const Color(0xFF0F8644), false));
+    meetings.add(Meeting('Voorbeeld 3', startTime.add(const Duration(hours: 24)), endTime.add(const Duration(hours: 24)) , const Color(0xFF0F8644), false));
+
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
