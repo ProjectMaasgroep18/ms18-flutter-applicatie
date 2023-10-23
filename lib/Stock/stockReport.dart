6616b0802d0f7fc4a341c5606f6456d77943b591
BMF010 <125911848+BMF010@users.noreply.github.com>
2023-10-23 18:06:46 +0200

    Update stockReport.dart
    dit is een commit

--- lib/Stock/stockReport.dart
+++ lib/Stock/stockReport.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ms18_applicatie/Models/stock.dart';
 import 'package:ms18_applicatie/Stock/widgets.dart';
 import 'package:ms18_applicatie/Widgets/paddingSpacing.dart';
@@ -13,6 +14,145 @@ class StockReport extends StatefulWidget {
 }
 
 class StockReportState extends State<StockReport> {
+  // Future<void> openOrderlinesDialog(code) async {
+  //   SystemChrome.setPreferredOrientations([
+  //     DeviceOrientation.portraitUp,
+  //     DeviceOrientation.portraitDown,
+  //   ]);
+  //   return showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return AlertDialog(
+  //           title: const Text(
+  //             "Pop-up",
+  //             style:  TextStyle(
+  //               fontWeight: FontWeight.bold,
+  //               color:mainColor,
+  //               fontSize: 20,
+  //             ),
+  //           ),
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(3.0)),
+  //           ),
+  //           content: Container(
+  //             width: double.minPositive,
+  //             // Set a minimum width to avoid intrinsic dimension issues
+  //             child: Row(
+  //               children: [
+  //                 Expanded(child: Container( child: TextFormField())
+  //                 )
+  //               ],
+  //             )
+  //
+  //           ),
+  //           actions: [
+  //             Row(
+  //               children: [
+  //                 Expanded(
+  //                   child: Container(
+  //                     child: ElevatedButton(
+  //                       onPressed: () {
+  //                         // Close Dialog
+  //                         dbHelper.syncOrders(ordernumber: code);
+  //                       },
+  //                       style: ElevatedButton.styleFrom(
+  //                         backgroundColor: const Color(awlGreen),
+  //                         padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
+  //                         shadowColor: const Color(awlGreen),
+  //                         elevation: 0,
+  //                       ),
+  //                       child: const FittedBox(
+  //                         fit: BoxFit.fill,
+  //                         child: Text(
+  //                           "Nog een keer versturen",
+  //                           style: TextStyle(
+  //                             fontSize: 14,
+  //                           ),
+  //                           textAlign: TextAlign.center,
+  //                         ),
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 ),
+  //                 SizedBox(width: 2),
+  //                 showOpen ? Expanded(
+  //                   child: Container(
+  //                     child: ElevatedButton(
+  //                       onPressed: () {
+  //                         // Close Dialog
+  //                         ordernumber = code;
+  //                         Navigator.push(
+  //                             context,
+  //                             MaterialPageRoute(
+  //                                 builder: (context) =>
+  //                                 const CreateOrderPage()));
+  //                       },
+  //                       style: ElevatedButton.styleFrom(
+  //                         backgroundColor: const Color(awlGreen),
+  //                         padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
+  //                         shadowColor: const Color(awlGreen),
+  //                         elevation: 0,
+  //                       ),
+  //                       child: const FittedBox(
+  //                         fit: BoxFit.fill,
+  //                         child: Text(
+  //                           "Openen order",
+  //                           style: TextStyle(
+  //                             fontSize: 14,
+  //                           ),
+  //                           textAlign: TextAlign.center,
+  //                         ),
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 ) : Container(),
+  //               ],
+  //             ),
+  //             Row(
+  //               children: [
+  //                 Expanded(
+  //                   child: Container(
+  //                     child: ElevatedButton(
+  //                       onPressed: () {
+  //                         orderlines = [];
+  //                         // Close Dialog
+  //                         Navigator.of(context, rootNavigator: true)
+  //                             .pop('dialog');
+  //                       },
+  //                       style: ElevatedButton.styleFrom(
+  //                         backgroundColor: const Color(awlGrey),
+  //                         padding: const EdgeInsets.fromLTRB(16, 5, 16, 5),
+  //                         shadowColor: const Color(awlGrey),
+  //                         elevation: 0,
+  //                       ),
+  //                       child: FittedBox(
+  //                         fit: BoxFit.fill,
+  //                         child: Text(
+  //                           closebtntxt,
+  //                           style: const TextStyle(
+  //                             fontSize: 14,
+  //                           ),
+  //                           textAlign: TextAlign.center,
+  //                         ),
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 )
+  //               ],
+  //             )
+  //           ],
+  //         );
+  //       }).then((value) {
+  //     Future.delayed(const Duration(milliseconds: 250), () {
+  //       SystemChrome.setPreferredOrientations([
+  //         DeviceOrientation.landscapeRight,
+  //         DeviceOrientation.landscapeLeft,
+  //         DeviceOrientation.portraitUp,
+  //         DeviceOrientation.portraitDown,
+  //       ]);
+  //     });
+  //   });
+  // }
   final List<StockProduct> stockProducts = [
     StockProduct(
       product: Product(
