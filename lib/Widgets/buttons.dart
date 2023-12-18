6f42d8e9eb6f62809a23338c6bba25a4253021d6
Pieter Visser <50768004+wasdafor@users.noreply.github.com>
2023-12-18 15:59:00 +0100

    Added the ability to add, update and remove products.

--- lib/Widgets/buttons.dart
+++ lib/Widgets/buttons.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:ms18_applicatie/config.dart';
 
@@ -6,21 +5,21 @@ class Button extends StatelessWidget {
   final IconData? icon;
   final String? text;
   final Function() onTap;
-  const Button({
-    super.key,
-    this.icon,
-    this.text,
-    required this.onTap,
-  });
+  final Color color;
+  const Button(
+      {super.key,
+      this.icon,
+      this.text,
+      required this.onTap,
+      this.color = mainColor});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
         padding: EdgeInsets.zero,
-        backgroundColor: mainColor,
+        backgroundColor: color,
         minimumSize: const Size(10, 48),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(
